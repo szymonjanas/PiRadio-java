55bf54565755511bd57bcd11d4ac6d985a94c50a
szymon <szymon.janas.95@gmail.com>
2020-05-05 18:31:58 +0200

    Add: Read and write radio station from/to file

---
+++ src/main/java/com/janas/PiRadio/Radio/RadioStations.java
@@ -0,0 +1,62 @@
+package com.janas.PiRadio.Radio;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RadioStations {
+
+    private static Map<String, String> stations = new HashMap<>();
+
+    private static File file = new File("radio-stations.txt");
+
+    public static String getStation(String name){
+        if (stations.size() == 0){
+            load();
+            save();
+        }
+        return stations.get(name);
+    }
+
+    public static void putStation(String name, String url){
+        stations.put(name, url);
+    }
+
+    public static void save() {
+        try {
+            FileWriter writer = new FileWriter(file);
+            StringBuilder builder = new StringBuilder();
+            for (Map.Entry entry: stations.entrySet())
+            {
+                builder.append(entry.getKey());
+                builder.append("\n");
+                builder.append(entry.getValue());
+                builder.append("\n");
+            }
+            writer.write(builder.toString());
+            writer.close();
+
+        } catch (Exception e){
+            System.out.println("Exception from save file: " + e.toString());
+        }
+    }
+
+    public static void load(){
+        try {
+            Scanner reader = new Scanner(file);
+            ArrayList<String> data = new ArrayList<>();
+            while (reader.hasNextLine()) {
+                data.add(reader.nextLine());
+            }
+            reader.close();
+            for (int i=0; i < data.size(); i+=2){
+                stations.put(data.get(i), data.get(i+1));
+            }
+        } catch (Exception e){
+            System.out.println("Exception from load file: " + e.toString());
+        }
+    }
+
+}
