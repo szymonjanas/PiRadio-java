b6ec1f5144a4e43c2dafc72629d20f650838506e
szymon <szymon.janas.95@gmail.com>
2020-05-09 23:19:54 +0200

    Add: new controller for simple site

---
+++ src/main/java/com/janas/PiRadio/Radio/RadioSiteController.java
@@ -0,0 +1,48 @@
+package com.janas.PiRadio.Radio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/radio")
+public class RadioSiteController {
+
+    private final RadioProcessor radioProcessor;
+
+    public RadioSiteController(RadioProcessor radioProcessor) {
+        this.radioProcessor = radioProcessor;
+    }
+
+    @GetMapping()
+    public ModelAndView index(Model model){
+        radioProcessor.index(model);
+        return new ModelAndView("radio");
+    }
+
+    @PostMapping("/on")
+    public ModelAndView turnOnRadio(@RequestParam String name){
+        radioProcessor.initRadio(name);
+        radioProcessor.turnOnRadio();
+        return new ModelAndView("redirect:/radio");
+    }
+
+    @PostMapping("/off")
+    public ModelAndView turnOffRadio(){
+        radioProcessor.turnOffRadio();
+        return new ModelAndView("redirect:/radio");
+    }
+
+    @PostMapping("/add")
+    public ModelAndView postStation(@ModelAttribute("station") RadioStationModel station){
+        radioProcessor.addStation(station);
+        return new ModelAndView("redirect:/radio");
+    }
+
+    @PostMapping("/delete")
+    public ModelAndView  deleteStation(@RequestParam String name){
+        radioProcessor.deleteStation(name);
+        return new ModelAndView("redirect:/radio");
+    }
+}
