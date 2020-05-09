033f2dff1d2d01d87e69b8b103437d5af960d7f9
szymon <szymon.janas.95@gmail.com>
2020-05-09 20:10:29 +0200

    Add: custom path to file in args[]

--- src/main/java/com/janas/PiRadio/PiRadioApplication.java
+++ src/main/java/com/janas/PiRadio/PiRadioApplication.java
@@ -4,11 +4,16 @@ import com.janas.PiRadio.Radio.RadioStations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.desktop.SystemSleepEvent;
+
 @SpringBootApplication
 public class PiRadioApplication {
 
 	public static void main(String[] args) {
+		for(int i = 0; i < args.length; i++) {
+			if (args[i].equals("--custom-stations-path"))
+				RadioStations.givenPath = args[i+1];
+		}
 		SpringApplication.run(PiRadioApplication.class, args);
 	}
-
 }
